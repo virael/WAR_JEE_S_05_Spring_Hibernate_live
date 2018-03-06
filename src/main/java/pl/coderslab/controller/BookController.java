47af6ce4781bfd32b34cac2f4095daf1d33ef959
Marcin Trościańczyk <Marcin.Troscianczyk@agora.pl>
2018-03-06 16:47:31 +0100

    06.03.2018 16:47

--- src/main/java/pl/coderslab/controller/BookController.java
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -1,99 +1,48 @@
 package pl.coderslab.controller;
 
-import java.util.Arrays;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import pl.coderslab.dao.BookDao;
-import pl.coderslab.entity.Author;
+import pl.coderslab.dao.PublisherDao;
 import pl.coderslab.entity.Book;
 import pl.coderslab.entity.Publisher;
 
-@RestController
+@Controller
 public class BookController {
-
+	
+	@Autowired
+	private PublisherDao publisherDao;
+	
 	@Autowired
 	private BookDao bookDao;
-
-	@GetMapping(path = "/book")
-	public Book findBookById() {
-		
-		final Book book = bookDao.findById(1);
-		
-		prepareBook(book);
-		
-		return book;
-	}
 	
-	@GetMapping(path = "/book/{rating}")
-	public Collection<Book> getRatingList(final @PathVariable("rating") byte rating) {
+	@GetMapping(path = "/book/add")
+	public String showAddBookForm(final Model model) {
 		
-		final Collection<Book> books = bookDao.getRatingList(rating);
-		
-		for(final Book book : books) {
-			prepareBook(book);
-		}
-		
-		return books;
-	}
-	
-	@PostMapping(path = "/book")
-	public Book addBook() {
-
 		final Book book = new Book();
-		book.setTitle("Elementarz");
+		model.addAttribute("book", book);
 		
-		final Author author = new Author();
-		author.setId(1L);
-		
-		book.setAuthors(Arrays.asList(author));
-		book.setDescription("Ala ma kota");
-		
-		final Publisher publisher = new Publisher();
-		publisher.setId(1L);
-		
-		book.setPublisher(publisher);
-		book.setRating((byte) 5);
+		return "book/add";		
+	}
+	
+	@PostMapping(path = "/book/add")
+	public String processAddBookForm(final Book book) {
+			
 		bookDao.add(book);
 		
-		return book;
-	}
-
-	@PutMapping(path = "/book")
-	public Book editBook() {
-		
-		final Book book = new Book();
-		book.setId(1L);
-		book.setRating((byte)9);
-		bookDao.edit(book);
-		
-		return book;
-	}
-
-	@DeleteMapping(path = "/book")
-	public void removeBookById() {
-		bookDao.removeById(1);
+		return "book/success";
 	}
 	
-	private void prepareBook(final Book book) {
-		
-		final Publisher publisher = book.getPublisher();
-		
-		if(publisher != null) {
-			publisher.setBooks(null);
-		}
-		
-		final Collection<Author> authors = book.getAuthors();
-		
-		for(final Author author : authors) {
-			author.setBooks(null);
-		}
+	@ModelAttribute("publishers")
+	public Collection<Publisher> publishers() {
+		final Collection<Publisher> publishers = publisherDao.findAll();
+		return publishers;
 	}
 }
